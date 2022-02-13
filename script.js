dd4e5bab2c760f579456a529c2a487899d1743ed
MHER <avoyanm@gmail.com>
2022-02-13 15:24:42 +0400

    Person, Student, Teacher classes

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ console.log(eli.getFullName());
 console.log(eli.getAnnularSalary());
 console.log(eli.raiseSalary(20));
 
+console.log("#####################################################");
+
 let charles = new Author("Charles Dickens", "charles@gmail.com", "Female");
 charles.gender = "male";
 console.log(charles.toString());
@@ -10,14 +12,32 @@ console.log(charles.toString());
 let citiesBook = new Book("A Tale of Two Cities", charles, 800, 200);
 console.log(citiesBook.toString());
 
+console.log("#####################################################");
+
 let acc1 = new Account(1, "Account_1", 200);
 acc1.credit(1000);
 console.log(acc1.toString());
 acc1.debit(200);
 console.log(acc1.toString());
+
 let acc2 = new Account(2, "Account_2", 550);
 acc1.transferTo(acc2, 250);
 console.log(acc1.toString());
 console.log(acc2.toString());
+
 let acc3 = new Account(1, "Account_1", 750);
 console.log(Account.identifyAccounts(acc1, acc3));
+
+console.log("#####################################################");
+
+let eliStud = new Student("Elliote", "Alderson", "Male", 19, ["math", "physics", "chemistry", "biology"], 1, 400);
+eliStud.passExam("math", 60);
+eliStud.passExam("physics", 60);
+eliStud.passExam("chemistry", 60);
+console.log(eliStud.toString());
+eliStud.passExam("biology", 60);
+console.log(eliStud.toString());
+
+let edTeach = new Teacher("Edward", "Alderson", "Male", 38, "Computer science", 120);
+console.log(edTeach.toString());
+console.log("#####################################################");
